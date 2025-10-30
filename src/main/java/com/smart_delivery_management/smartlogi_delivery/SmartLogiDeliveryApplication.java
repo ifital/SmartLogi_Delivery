4bd8854a573948bc9f03accdbe919aecdff6e645
Latifi777 <abdelalilatifi3@gmail.com>
2025-10-30 11:55:17 +0100

    first commit

---
+++ src/main/java/com/smart_delivery_management/smartlogi_delivery/SmartLogiDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.smart_delivery_management.smartlogi_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLogiDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLogiDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smart_delivery_management/smartlogi_delivery/SmartLogiDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_delivery_management.smartlogi_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLogiDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
